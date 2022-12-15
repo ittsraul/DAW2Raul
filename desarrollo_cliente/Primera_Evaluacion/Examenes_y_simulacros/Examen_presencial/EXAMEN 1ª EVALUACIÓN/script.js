c37de6b8ff5329dc8e41f6ad6c83f93492cf9ae8
ittsraul <raulsahe@protonmail.com>
2022-12-15 19:02:48 +0100

    ejercicio5

--- desarrollo_cliente/Primera_Evaluacion/Examenes_y_simulacros/Examen_presencial/EXAMEN 1ª EVALUACIÓN/script.js
+++ desarrollo_cliente/Primera_Evaluacion/Examenes_y_simulacros/Examen_presencial/EXAMEN 1ª EVALUACIÓN/script.js
@@ -1,28 +1,27 @@
-CambiaVisibilidad();
+//CambiaVisibilidad();
 Api();
 
 //CambiaVisibilidad
-function CambiaVisibilidad() {
-  let p2 = document.getElementById("parrafoDos");
-  let p3 = document.getElementById("parrafoTres");
-  let p4 = document.getElementById("parrafoCuatro");
+//function CambiaVisibilidad() {
+let p1 = document.getElementById("parrafoUno")
+let p1Content = p1.innerHTML;
+let p2 = document.getElementById("parrafoDos");
+let p3 = document.getElementById("parrafoTres");
+let p4 = document.getElementById("parrafoCuatro");
 
-  p2.style.visibility = "hidden";
-  p3.style.visibility = "hidden";
-  p4.style.visibility = "hidden";
-}
+p2.style.visibility = "hidden";
+p3.style.visibility = "hidden";
+p4.style.visibility = "hidden";
+//}
 
 //Array Contenedor
 let container = [];
-
-//Contador
-let contador = 0;
 
 //Conexion a la api
 async function Api() {
   try {
     let response = await axios.get("https://rickandmortyapi.com/api/character");
-     console.log(response.data.results); 
+    /* console.log(response.data.results); */
     container.push(response.data.results[0]);
     /* console.log(container); */
     /* console.log(container[0]); */
@@ -30,15 +29,40 @@ async function Api() {
     container.push(response.data.results[1]);
     /* console.log(container[0]); */
     container.push(response.data.results[11]);
-      console.log(container); 
+    console.log(container);
     container.push(response.data.results[12]);
     /* console.log(container[0].status); */
     let imagen = document.getElementsByTagName("h1")[0];
-     imagen.style.background = "url(" + container[0].image + ") no-repeat"; 
-     imagen.addEventListener("mouseover", ()=>{
-      
-     });
-  } catch (error) {
-    console.error(error);
-  }
+    imagen.style.background = "url(" + container[0].image + ") no-repeat";
+    imagen.addEventListener("mouseover", () => {
+      imagen.style.background = "url(" + container[1].image + ") no-repeat";
+      imagen.style.color = "red";
+      imagen.style.textAlign = "left";
+      imagen.innerHTML = container[1].name;
+      p1.innerHTML = p2.innerHTML;
+
+      imagen.addEventListener("click", () => {
+        imagen.style.background = "url(" + container[2].image + ") no-repeat";
+        imagen.style.color = "red";
+        imagen.style.textAlign = "left";
+        imagen.innerHTML = container[2].name;
+        p1.innerHTML = p3.innerHTML;
+        imagen.addEventListener("click", () => {
+          imagen.style.background = "url(" + container[3].image + ") no-repeat";
+          imagen.style.color = "red";
+          imagen.style.textAlign = "left";
+          imagen.innerHTML = container[3].name;
+          p1.innerHTML = p4.innerHTML;
+        })
+
+      });
+    });
+  imagen.addEventListener("mouseout", () => {
+    imagen.style.background = "url(" + container[0].image + ") no-repeat";
+    imagen.innerHTML = "";
+    p1.innerHTML = p1Content.innerHTML;
+  });
+} catch (error) {
+  console.error(error);
+}
 }
--- desarrollo_cliente/Primera_Evaluacion/Examenes_y_simulacros/Examen_presencial/EXAMEN 1ª EVALUACIÓN/script5.js
+++ desarrollo_cliente/Primera_Evaluacion/Examenes_y_simulacros/Examen_presencial/EXAMEN 1ª EVALUACIÓN/script5.js
@@ -1,75 +1,46 @@
+let anyadirALaLista = document.getElementsByTagName("form")[0];
 
-borrado();
-// dinamicDiv();
-
-function borrado(){
-    let VerSeries = document.getElementsByTagName("button")[1];
-    VerSeries.addEventListener("click", ()=>{
-        let Cuerpo = document.getElementsByTagName("article")[0];
-        let form1 = document.getElementsByTagName("form")[0];
-        let form2 = document.getElementsByTagName("form")[0];
-        Cuerpo.removeChild(form1);
-        Cuerpo.removeChild(form2);
-        
-    })
-}
-
-function dinamicDiv() {
-    let p = document.createElement("p");
-    tabla.appendChild(p);
-    let imagen = document.createElement("img");
-    tabla.appendChild(imagen);
-    imagen.style.width = "400px";
-    imagen.style.height = "500px";
-    let antes = document.createElement("button");
-    tabla.appendChild(antes);
-    let despues = document.createElement("button");
-    tabla.appendChild(despues);
-    antes.innerHTML = "anterior";
-    tabla.appendChild(antes)
-    despues.innerHTML = "despues";
-    tabla.appendChild(despues)
-}
+anyadirALaLista.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let serie = document.getElementsByTagName("input")[0].value;
+  let foto = document.getElementsByTagName("input")[1].value;
+  window.localStorage.setItem(serie, foto)
+});
 
 
+console.log(foto.value);
+let VerSeries = document.getElementsByTagName("button")[1];
+VerSeries.addEventListener("click", (e) => {
+  e.preventDefault();
+  let Cuerpo = document.body;
+  let form1 = document.getElementsByTagName("form")[0];
+  form1.remove();
+  let form2 = document.getElementsByTagName("form")[0];
+  form2.remove();
+  let p = document.createElement("p");
+  Cuerpo.appendChild(p);
+  let imagen = document.createElement("img");
+  Cuerpo.appendChild(imagen);
+  imagen.style.width = "400px";
+  imagen.style.height = "500px";
+  let antes = document.createElement("button");
+  Cuerpo.appendChild(antes);
+  let despues = document.createElement("button");
+  Cuerpo.appendChild(despues);
+  antes.innerHTML = "anterior";
+  Cuerpo.appendChild(antes)
+  despues.innerHTML = "despues";
+  Cuerpo.appendChild(despues);
+  /* antes.addEventListener("click", () => {
 
-/* function CarruselStorage(antes, despues){
-    let contador = 0;
-    antes.addEventListener("click", ()=>{
-        contador--;
-
-
-    });
-} */
-
-/*
-//Función asyncrona que conecta a la Api
-
-
-//Función con lógica de botones.
-function boton(antes, despues) {
-  antes.addEventListener("click", () => {
-    if (contador > 0) {
-      contador--;
-    } else {
-      contador = Maxres;
-    }
-    Api();
-  });
-
+    p.innerHTML = window.localStorage.getItem();
+  })
+  
   despues.addEventListener("click", () => {
-    if (contador < Maxres) {
-      elem++;
-    } else {
-      elem = 0;
-    }
-  });
-}
+    p.innerHTML = "";
+  }) */
+})
 
-function MuestraElementos(contador){
-    let p = document.getElementsByTagName("p")[0];
-    let img = document.getElementsByTagName("img")[0];
-    p.innerHTML = "Nombre:" + response[contador]["name"] + " Actor:" + response[contador]["portrayed"] + " Rol:" + response[contador]["ocupation"];
-    img.src = response[contador]["img"]
-}
- */
+
+
+
